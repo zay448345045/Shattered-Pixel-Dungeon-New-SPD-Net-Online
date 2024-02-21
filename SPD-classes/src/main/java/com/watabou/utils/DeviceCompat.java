2e64a4b066ef8b5cccad198dcdb00320d5e54dca
catandA <75292299+catandA@users.noreply.github.com>
2024-02-21 11:48:29 +0800

    修复isDebug的判定

--- SPD-classes/src/main/java/com/watabou/utils/DeviceCompat.java
+++ SPD-classes/src/main/java/com/watabou/utils/DeviceCompat.java
@@ -60,7 +60,7 @@ public class DeviceCompat {
 	}
 	
 	public static boolean isDebug(){
-		return Game.version.contains("INDEV");
+		return Game.netVersion.contains("INDEV");
 	}
 	
 	public static void log( String tag, String message ){
