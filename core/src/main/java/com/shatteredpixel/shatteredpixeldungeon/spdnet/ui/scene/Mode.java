56eac9efbc73a5c2624c07ca4a5c882316676bff
catandA <75292299+catandA@users.noreply.github.com>
2024-02-28 17:57:48 +0800

    修复图标不正确显示问题

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/spdnet/ui/scene/Mode.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/spdnet/ui/scene/Mode.java
@@ -5,19 +5,31 @@ import com.watabou.noosa.Image;
 
 import lombok.Getter;
 
-@Getter
 public enum Mode {
-	IRONMAN("铁人模式", "没有玩家之间的实质性交互，使用你的技巧争夺排行榜上的高分，证明自己是地牢高手。", new Image(Assets.Sprites.RATKING, 0, 0, 16, 16)),
-	FUN("娱乐模式", "随便摸，乐就行了。 : )", new Image(Assets.Sprites.RAT, 0, 0, 16, 16)),
-	DAILY("每日挑战", "每天都有新的地牢，每天都有新排行榜", new Image(Assets.Sprites.RAT, 0, 15, 16, 16)),
-	;
+	IRONMAN("铁人模式", "没有玩家之间的实质性交互，使用你的技巧争夺排行榜上的高分，证明自己是地牢高手。", Assets.Sprites.RATKING, 0, 0, 16, 16),
+	FUN("娱乐模式", "随便摸，乐就行了。 : )", Assets.Sprites.RAT, 0, 0, 16, 16),
+	DAILY("每日挑战", "每天都有新的地牢，每天都有新排行榜", Assets.Sprites.RAT, 0, 15, 16, 16);
+	@Getter
 	private final String name;
+	@Getter
 	private final String description;
-	private final Image icon;
+	private final String icon;
+	private final int iconLeft;
+	private final int iconTop;
+	private final int iconWidth;
+	private final int iconHeight;
 
-	Mode(String name, String description, Image icon) {
+	Mode(String name, String description, String icon, int iconLeft, int iconTop, int iconWidth, int iconHeight) {
 		this.name = name;
 		this.description = description;
 		this.icon = icon;
+		this.iconLeft = iconLeft;
+		this.iconTop = iconTop;
+		this.iconWidth = iconWidth;
+		this.iconHeight = iconHeight;
+	}
+
+	public Image getIcon() {
+		return new Image(icon, iconLeft, iconTop, iconWidth, iconHeight);
 	}
 }
