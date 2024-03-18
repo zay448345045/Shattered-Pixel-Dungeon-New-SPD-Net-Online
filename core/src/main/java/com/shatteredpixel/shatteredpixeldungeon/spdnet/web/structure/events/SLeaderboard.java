227bce570bd9aa29daff03e90ea23e139fb58fee
catandA <75292299+catandA@users.noreply.github.com>
2024-03-18 18:06:31 +0800

    实现服务端发送排行榜

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/spdnet/web/structure/events/SLeaderboard.java
@@ -0,0 +1,21 @@
+package com.shatteredpixel.shatteredpixeldungeon.spdnet.web.structure.events;
+
+import com.shatteredpixel.shatteredpixeldungeon.spdnet.web.structure.Data;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SLeaderboard extends Data {
+	private int totalPages;
+	private int currentPage;
+	private int totalElements;
+	private List<String> gameRecords;
+}
