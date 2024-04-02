37666a6ae2413f683ed5bbb76afd90c4521e8743
catandA <75292299+catandA@users.noreply.github.com>
2024-04-02 23:26:14 +0800

    长期存储key

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/spdnet/NetSettings.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/spdnet/NetSettings.java
@@ -1,14 +1,20 @@
 package com.shatteredpixel.shatteredpixeldungeon.spdnet;
 
-import lombok.Getter;
-import lombok.Setter;
+import static com.watabou.utils.GameSettings.getString;
+import static com.watabou.utils.GameSettings.put;
 
 /**
  * 用来存储某些长期保存的变量
  */
 public class NetSettings {
-	@Getter
-	@Setter
-	private static String key = "default";
+	public static String DEFAULT_KEY = "debug";
+	public static final String KEY_AUTH_KEY = "net_auth_key";
 
+	public static void setKey(String value) {
+		put(KEY_AUTH_KEY, value);
+	}
+
+	public static String getKey() {
+		return getString(KEY_AUTH_KEY, DEFAULT_KEY);
+	}
 }
